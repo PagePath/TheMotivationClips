d3e1322a8a16566c8a4af1eb5366d84a3c1f6de0
Page Path <129186795+PagePath@users.noreply.github.com>
2023-04-18 19:50:27 +0530

    Add files via upload

---
+++ linktree.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", function() {
+  var header = document.getElementById("myHeader");
+  if (window.pageYOffset > 100) {
+    header.classList.add("show");
+  } else {
+    header.classList.remove("show");
+  }
+});
+
+
+
